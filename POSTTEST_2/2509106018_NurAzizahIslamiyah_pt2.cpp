a8f9d8a15cb014297a2b41f8688eff5c590402c3
Nur Azizah Islamiyah <nrislamiyah14@gmail.com>
2026-03-19 12:34:10 +0800

    genap

---
+++ POSTTEST_2/2509106018_NurAzizahIslamiyah_pt2.cpp
@@ -0,0 +1,9 @@
+// Sistem manajemen keberangkatan Kereta Api
+// Program mengelola jadwal dan informasi tiket kereta api (struct, pointer, array of struct, searching, dan sorting)
+// Tampil jadwal semua kereta (menggunakan pointer)
+// Tambah data kereta baru
+// Linear Search cari berdasarkan rute (asal & tujuan)
+// jump Search cari berdasarkan nomor kereta
+// Merge sort urutkan berdasarkan nama kereta (A-Z)
+// Selection Sort urutkan berdasarkan harga tiket (termurah)
+// Setiap searching dan pointer menerapkan swap dengan pointer
