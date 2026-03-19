dafcfc9574d2faa290e4e3b581f3baf19db5f9d3
Nur Azizah Islamiyah <nrislamiyah14@gmail.com>
2026-03-19 23:57:10 +0800

    data

--- POSTTEST_2/2509106018_NurAzizahIslamiyah_pt2.cpp
+++ POSTTEST_2/2509106018_NurAzizahIslamiyah_pt2.cpp
@@ -13,12 +13,21 @@
 using namespace std;
 
 struct Kereta {
-    int noKereta;
-    string namaKereta;
+    int no_kereta;
+    string nama_kereta;
     string asal;
     string tujuan;
     int harga;
 };
+
+Kereta daftar_kereta[100] = {
+    {101, "Thomas Rail", "Jakarta", "Surabaya", 500000},
+    {112, "Emily Emerald", "Malang", "Solo", 600000},
+    {110, "Titipo Little", "Yogyakarta", "Jakarta", 450000},
+    {104, "Gordon Big", "Bandung", "Solo", 480000},
+    {106, "Percy GreenLoco", "Surabaya", "Bogor", 250000}
+};
+int jumlah_kereta = 5;
 
 void swap(Kereta* a, Kereta* b) {
     Kereta temp = *a;
